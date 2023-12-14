520b2ec0e8b89acba8ef50b865bd0de46dba2c0e
Benjamin Loison <benjamin_loison@users.noreply.gitea.lemnoslife.com>
2023-12-14 03:50:15 +0100

    Add user sentence embedding as centroid of likes

--- learn_sent_embeddings.py
+++ learn_sent_embeddings.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from sentence_transformers import SentenceTransformer
 import pickle
+import csv
 
 # descriptive file in the format 'id_item,text'
 dataset = 'movielens'
@@ -19,22 +20,36 @@ model = SentenceTransformer(sentenceTransformerName)
 embeddings = []
 
 # encoding sentences
-for cont in descriptions:
+for contIndex, cont in enumerate(descriptions):
+  print('encoding sentence', contIndex, len(descriptions))
 
   try:
     embedding = model.encode(cont)
     embeddings.append(embedding)
   except:
-    embedding = np.zeros(len(embedding))
-    embeddings.append(embedding)
+    print('error')
+    exit(1)
+
+rows = []
+with open(f'{dataset}/train.tsv') as tsvFile:
+    reader = csv.reader(tsvFile, delimiter='\t')
+    rows = list(reader)
+
+usersSet = set()
+for row in rows:
+    userId = int(row[0])
+    usersSet.add(userId)
+usersCount = len(usersSet)
+
+users = {}
+for row in rows:
+    userId, itemId, dis_like = [int(field) for field in row]
+    if dis_like == 1:
+        print(itemId, usersCount)
+        users[userId] = users.get(userId, []) + [embeddings[itemId - usersCount]]
 
 # create dictionary
-dictionary = {}
-
-i = 0
-while i < len(ids):
-  dictionary[i] = embeddings[i]
-  i += 1
+dictionary = {userId: sum(users[userId]) / len(users[userId]) for userId in users} | {i + usersCount: embeddings[i] for i in range(len(ids))}
 
 # save embeddings
 pickle.dump(dictionary, open(f'{dataset}_{sentenceTransformerName}.pickle', 'wb'))
